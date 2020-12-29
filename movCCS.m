94c72c0662a0f679de7afe050cb2f49bcd501d9d
lbreston <leo.breston@gmail.com>
2020-12-29 00:46:39 -0800

    removed wrapper functions

--- movCCS.m
+++
@@ -1,3 +0,0 @@
-function Y = movCCS(X,l,tau,dim,k,o)
-Y=movfun(@(w,z)CCS(w,z,l,tau,dim),X,k,o);
-end
--- pwCCS.m
+++
@@ -1,5 +0,0 @@
-function Y=pwCCS(X,l,tau,dim)
-triSymOut={@(c)num2cell(c,2),@(d,e)sub2ind(e,[d(1) d(2)],[d(2) d(1)])};
-Y=tuplefun(@(w,z)CCS(w,z,l,tau,dim),X,1,false,triSymOut);
-end
-
